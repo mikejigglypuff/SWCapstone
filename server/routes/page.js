9b4c05391d86d9426527a90585bdeec3f28e6601
김광민 <kkm7060353@naver.com>
2023-05-11 15:39:34 +0900

    model 구현

--- server/routes/page.js
+++ server/routes/page.js
@@ -1,6 +1,7 @@
 //라우팅 기능 구현 모듈
 
 const express = require("express");
+var User = require('../models/users');
 const { renderMain, renderBoard, renderLogin, renderFindID, 
   renderJoin, renderFindPW, renderReplacePW} = require("../controller/page");
 
@@ -13,5 +14,13 @@ router.get("/join", renderJoin);
 router.get("/findID", renderFindID);
 router.get("/findPW", renderFindPW);
 router.get("/replacePW", renderReplacePW);
+router.get("/users", (req, res) => {
+  User.find({where:{id:1}})
+      .then((user) => {
+    res.render('index', {
+      title: 'Express',
+      useremail: "1234@mail.com"});
+  });
+});
 
 module.exports = router;
