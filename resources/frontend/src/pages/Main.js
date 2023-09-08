b88f42ddf06c3c7d2cd2a97c7e1b4cb4ee5e76aa
castlehuni <osh8137@naver.com>
2023-09-08 22:23:08 +0900

    add react and make header

---
+++ resources/frontend/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+const Main = () => {
+    return(
+        <div>
+            <div className="video">
+                <iframe width="900" height="500" src="https://www.youtube.com/embed/BIEezW7aPRw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard_summ-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div><br></br>
+        </div>
+    );
+}
+export default Main
