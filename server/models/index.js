843d3ab4bc40b9120fbba329673bccc5a0ece2e7
purin <k24555569@gmail.com>
2023-09-12 00:51:31 +0900

    RDS 적용

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,19 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect
+});
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("RDS 연결 완료");
+    })
+    .catch((e) => {
+        console.error(e);
+    });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
