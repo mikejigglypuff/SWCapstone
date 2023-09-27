e3917918097bf30748229f95bde48f9f4aa7f888
김광민 <kkm7060353@naver.com>
2023-09-27 15:12:26 +0900

    comments table

--- server/models/users.js
+++ server/models/users.js
@@ -1,17 +1,31 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('users', {
-    phonenumber: {
+    user_id: {
       type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING(45),
       allowNull: false
+    },
+    phonenumber: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+      unique: true
     },
     password: {
       type: DataTypes.STRING(45),
       allowNull: false
     },
-    username: {
-      type: DataTypes.STRING(45),
+    user_Num: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       allowNull: false
+    },
+    reg_Date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
     }
   }, {
     sequelize,
