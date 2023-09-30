58dc9cba7761822f909b11a8d09c11a704a20adc
purin <k24555569@gmail.com>
2023-09-30 20:44:37 +0900

    add comment funcs

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -47,32 +47,29 @@ exports.getPostByCategory = async (req, res, next) => {
 }; //게시판 별 게시글 조회
 
 exports.postPost = async (req, res, next) => {
-  const t = DB.sequelize.transaction();
-
   const userId = await DB.Users.findOne({
     attributes: ['user_id'],
     where: {
       username: req.body.name
     }
-  }, { 
-    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
-    lock: true,
-    transaction: t 
   });
 
   if(!userId) { 
-    await t.rollback();
     errRes(res, 404, "user not found"); 
   }
 
+  const t = DB.sequelize.transaction();
   try {
     const post = await DB.Posts.create({
       title: req.body.title,
       content: req.body.content,
+      category: req.body.category,
       userId: userId,
-      favcnt: 0,
-      createdAt: Sequelize.NOW
-    }, { transaction: t });
+      favcnt: 0
+    }, { 
+      lock: true,
+      transaction: t
+    });
 
     t.afterCommit(() => {
       console.log(post);
@@ -88,29 +85,26 @@ exports.postPost = async (req, res, next) => {
 }; //게시글 등록
 
 exports.deletePost = async (req, res, next) => {
-  const t = DB.sequelize.transaction();
-
   const post = await DB.Posts.findOne({
     where: {
       post_id: req.body.id
     }
-  }, { 
-    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
-    lock: true,
-    transaction: t 
   });
 
   if(post) { 
-    await t.rollback();
     errRes(res, 404, "page not found"); 
   }
 
+  const t = DB.sequelize.transaction();
   try {
     await DB.Posts.destroy({
       where: {
         post_id: req.body.id
       }
-    }, { transaction: t });
+    }, { 
+      lock: true,
+      transaction: t 
+    });
 
     t.afterCommit(() => {
       res.status(302).redirect("/");
@@ -125,23 +119,17 @@ exports.deletePost = async (req, res, next) => {
 }; //게시글 삭제
 
 exports.patchPost = async (req, res, next) => {
-  const t = DB.sequelize.transaction();
-
   const post = await DB.Posts.findOne({
     where: {
       post_id: req.body.id
     }
-  }, { 
-    isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
-    lock: true,
-    transaction: t 
   });
 
   if(post.length) { 
-    await t.rollback();
     errRes(res, 404, "page not found"); 
   }
 
+  const t = DB.sequelize.transaction();
   try {
     await DB.Posts.update({
       title: req.body.title,
