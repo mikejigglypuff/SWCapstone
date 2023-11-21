65650478562f0bd2469579a2cc4018a03660fa20
purin <k24555569@gmail.com>
2023-11-21 20:51:40 +0900

    관리자 페이지 인증 수정

--- server/authCheck.js
+++ server/authCheck.js
@@ -1,16 +1,15 @@
 const { Op } = require("sequelize");
 const DB = require("./models/index");
 
-module.exports = {
-  //회원 로그인 여부 체크
-  hasSession: (req, res) => {
-    return (req.session && req.session.is_Logined);
-  },
-
-  //관리자 여부 체크
-  isAdmin: (req, res) => {
-    if(req.session && req.session.is_Admin) { return "admin"; }
-    else if(this.hasSession(req, res)) { return "user"; }
-    return "";
-  },
+//회원 로그인 여부 체크
+exports.hasSession = (req, res) => {
+  return (req.session && req.session.is_Logined);
 }
+
+//관리자 여부 체크
+exports.isAdmin = (req, res) => {
+  if(req.session && req.session.is_Admin) { return "admin"; }
+  else if(this.hasSession(req, res)) { return "user"; }
+  return "";
+}
+
