1f9fb201eb2f3d43509db9a819f53a9cd99b1d13
castlehuni <osh8137@naver.com>
2023-12-11 22:46:12 +0900

    add unregister function

--- resources/frontend/src/pages/ReplacePW.js
+++ resources/frontend/src/pages/ReplacePW.js
@@ -66,6 +66,7 @@ const ReplacePW = () => {
                 "pw": beforePW,
                 "newPW": newPW
             });
+            alert("비밀번호 변경이 완료되었습니다.");
             deleteSession();
             navigate('/login');
         }catch(error){
